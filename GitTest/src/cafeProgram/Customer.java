1f3c0c0206b51e4320e601929af0b547992a0b86
irruyan <irruyan@gmail.com>
2025-02-05 13:59:05 +0900

    쿠폰 추가

---
+++ GitTest/src/cafeProgram/Customer.java
@@ -0,0 +1,52 @@
+package cafeProgram;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+import lombok.Data;
+
+@Data
+public class Customer  implements Serializable{
+
+		private static final long serialVersionUID = 1L;
+		
+	  	private String id;
+	   	private String pw;
+	   	private int stamp = 0;
+	   	private int coupon = 0;
+
+	    public Customer(String id, String pw) {
+	        this.id = id;
+	        this.pw = pw;
+	    }
+
+	    public String getId() {
+	        return id;
+	    }
+
+	    public String getPw() {
+	        return pw;
+	    }
+
+		@Override
+		public String toString() {
+			return "[" + "아이디 : " + id + ", " +  "비밀번호 : " + pw + "]";
+		}
+
+		@Override
+		public boolean equals(Object obj) {
+			if (this == obj)
+				return true;
+			if (obj == null)
+				return false;
+			if (getClass() != obj.getClass())
+				return false;
+			Customer other = (Customer) obj;
+			return Objects.equals(id, other.id);
+		}	
+		
+		@Override
+		public int hashCode() {
+		    return Objects.hash(id);
+		}
+}
